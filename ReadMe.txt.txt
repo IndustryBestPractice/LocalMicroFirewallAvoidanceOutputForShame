d8f00d6bbc505775809c950d467e1ed63f6e87dd
IndustryBestPractice <IndustryBestPractice2@gmail.com>
2021-11-12 23:38:28 -0500

    Added dev howto files
    Added howto files. Will just recreate report when finished because JESUS CHRIST  i'm bad at development where I take months off in between work

---
+++ ReadMe.txt.txt
@@ -0,0 +1,65 @@
+# How to test and develop
+
+# Step 1 - create the folder to house your firewall log files
+sudo mkdir /lmfao/golang/data -p
+sudo mkdir /lmfao/mariadb/data -p
+
+# Step 2 - Create network for all of this to talk in
+# create a netwok
+sudo docker network create lmfao-network
+
+# Step 2.5  pull powershell
+sudo docker pull mcr.microsoft.com/powershell
+sudo docker run -it --rm --network lmfao-network --name powershell-console mcr.microsoft.com/powershell
+
+# Step 3 - pull the golang container, then start it while mounting the data dir
+sudo docker pull golang
+sudo docker run --rm --name golang -v /lmfao/golang/data:/data -it golang
+
+# Step 4 - start the mariadb instance that we will use to store the data
+sudo docker pull mariadb
+# Stert the container in headless mode
+sudo docker run --name mariadb --rm --detach --network lmfao-network -v /lmfao/mariadb/data:/data --env MARIADB_USER=lmfao-user --env MARIADB_PASSWORD=my_cool_secret --env MARIADB_ROOT_PASSWORD=my-secret-pw mariadb
+
+# Start another container in interactive mode to run SQL statements as needed
+#sudo docker run -it --network lmfao-network --name mariadb-console --rm mariadb mysql -hmariadb -ulmfao-user -p
+# PASSWORD TO USE ABOVE WHEN LOGGING IN IS my_cool_secret
+
+sudo docker run -it --network lmfao-network --name mariadb-console --rm mariadb mysql -hmariadb -uroot -p
+# PASSWORD TO USE ABOVE WHEN LOGGING IN IS my-secret-pw
+# to get shell access to the mariadb container to pull logs, etc run: docker exec -it mariadb bash
+
+# Step 5 - Create the database that we will be using
+CREATE DATABASE lmfao;
+use lmfao;
+create table ipv4(
+   ipv4_id INT NOT NULL AUTO_INCREMENT,
+   ipv4_address VARCHAR(100) NOT NULL,
+   rfc1918 bool NOT NULL,
+   PRIMARY KEY ( ipv4_id )
+);
+create table ipv6(
+   ipv6_id INT NOT NULL AUTO_INCREMENT,
+   ipv6_address VARCHAR(100) NOT NULL,
+   rfc1918 bool NOT NULL,
+   PRIMARY KEY ( ipv6_id )
+);
+create table transaction(
+   trans_id INT NOT NULL AUTO_INCREMENT,
+   src_ip_id INT NULL,
+   dst_ip_id INT NULL,
+   datetime datetime NOT NULL,
+   action VARCHAR(100) NOT NULL,
+   protocol VARCHAR(100) NOT NULL,
+   srcport VARCHAR(100) NOT NULL,
+   dstport VARCHAR(100) NOT NULL,
+   path VARCHAR(100) NOT NULL,
+   PRIMARY KEY ( trans_id ),
+   index trans_search_ip (src_ip_id, dst_ip_id),
+   index trans_search_dstip (dst_ip_id),
+   index trans_search_ip_proto (src_ip_id, dst_ip_id, protocol),
+   index trans_search_ip_ports (src_ip_id, dst_ip_id, srcport, dstport),
+   index trans_search_ports (srcport, dstport),
+   index trans_search_dstport (dstport)
+);
+GRANT ALL PRIVILEGES ON lmfao TO 'lmfao-user';
---
+++ create_visjs_files.ps1
@@ -0,0 +1,123 @@
+﻿cls
+# Generate nodes
+$nodes = @{}
+$nodes['nodes'] = @()
+
+# Value below affects size of node
+# Group below affect color of node
+
+$var = @{id = 1; label = "Kevin"; title = "Kevin Roman"; value = 21; group = 24
+    # Can also add components in here for position on the map once it's generated
+    #x = "717.32806",
+    #y = "699.96234",
+}
+$nodes['nodes'] += $var
+$var = @{id = 2; label = "Lauren"; title = "Lauren Salopek"; value = 21; group = 11}
+$nodes['nodes'] += $var
+$var = @{id = 3; label = "Roro"; title = "Rory Roman"; value = 22; group = 6}
+$nodes['nodes'] += $var
+
+$javascript_nodes = $nodes | ConvertTo-Json
+$javascript_nodes = $($javascript_nodes[1..$($javascript_nodes.length - 2)] -join "").trim() -replace '"nodes":','var nodes ='
+
+
+# Generate nodes
+$edges = @{}
+$edges['edges'] = @()
+
+# Value scales thckness of the line
+# title is the "hover over" value
+# label is the permanent view value
+
+$var = @{from = 1; label = "Hello\nWorld!"; to = 2; value = 5}
+$edges['edges'] += $var
+$var = @{from = 1; to = 2; value = 2; title = "partner"; color = "black"}
+$edges['edges'] += $var
+$var = @{from = 1; to = 3}
+$edges['edges'] += $var
+$var = @{from = 2; to = 3}
+$edges['edges'] += $var
+
+$javascript_edges = $edges | ConvertTo-Json
+$javascript_edges = $($javascript_edges[1..$($javascript_edges.length - 2)] -join "").trim() -replace '"edges":','var edges ='
+
+$javascript_nodes
+$javascript_edges
+
+remove-item "C:\users\kroman\Desktop\datasources\test_source.js" -Force
+Add-Content -Path "C:\users\kroman\Desktop\datasources\test_source.js" -Value $javascript_nodes
+Add-Content -Path "C:\users\kroman\Desktop\datasources\test_source.js" -Value ""
+Add-Content -Path "C:\users\kroman\Desktop\datasources\test_source.js" -Value $javascript_edges
+Get-Content "C:\users\kroman\Desktop\datasources\test_source.js"
+
+$main_file = @'
+<html>
+<head>
+    <script type="text/javascript" src="https://unpkg.com/vis-network/standalone/umd/vis-network.min.js"></script>
+	<script src="./datasources/test_source.js"></script>
+	<!--<script src="file://users/kroman/desktop/datasources/test_source.js"></script>-->
+
+    <style type="text/css">
+        #mynetwork {
+            width: 600px;
+            height: 400px;
+            border: 1px solid lightgray;
+        }
+    </style>
+</head>
+<body>
+<div id="mynetwork"></div>
+
+<script type="text/javascript">
+	// create an array with nodes
+	var nodesDataset = new vis.DataSet(nodes);
+	// create an array with nodes
+	var edgesDataset = new vis.DataSet(edges);
+    // create a network
+    var container = document.getElementById('mynetwork');
+	
+        var options = {
+          nodes: {
+            shape: "dot",
+            scaling: {
+              min: 10,
+              max: 30,
+              label: {
+                min: 8,
+                max: 30,
+                drawThreshold: 12,
+                maxVisible: 20,
+              },
+            },
+            font: {
+              size: 12,
+              face: "Tahoma",
+            },
+          },
+          edges: {
+            width: 0.15,
+            color: { inherit: "from" },
+            smooth: {
+              type: "continuous",
+            },
+          },
+          physics: false,
+          interaction: {
+            tooltipDelay: 200,
+            hideEdgesOnDrag: true,
+            hideEdgesOnZoom: true,
+          },
+        };
+
+    // provide the data in the vis format
+	var data = { nodes: nodesDataset, edges: edgesDataset };
+
+    // initialize your network!
+    var network = new vis.Network(container, data, options);
+</script>
+</body>
+</html>
+'@
+
+remove-item "C:\users\kroman\Desktop\test.html" -Force
+Add-Content -Path "C:\users\kroman\Desktop\test.html" -Value $main_file
