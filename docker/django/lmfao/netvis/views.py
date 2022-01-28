29ac2c25f31a84a4836a42771c4904247891c1f5
IndustryBestPractice <IndustryBestPractice2@gmail.com>
2022-01-28 01:22:32 -0500

    added node labels

--- docker/django/lmfao/netvis/views.py
+++ docker/django/lmfao/netvis/views.py
@@ -7,6 +7,7 @@ from django.db.models import Count
 from .models import Events,IPAddress
 
 import datetime
+import random
 
 # Create your views here.
 def index(request):
@@ -33,10 +34,37 @@ def index(request):
                 union_query.count()
                 # Create a list of all node ids
                 node_ids = []
+                legend_nodes = []
+                legend_colors = {}
                 for row in union_query:
                     node_ids.append(row['src_ip_id'])
                 # Get the IPAddress data for each ip_id in Events table
                 nodes = IPAddress.objects.filter(id__in=node_ids).values('id','ip_address','cidr','hostname')
+                unique_cidrs = nodes.all().values_list('cidr').distinct().values('cidr')
+                rando_color = lambda: random.randint(0,255)
+                last_node_id = nodes.all().values('id').order_by('id').last()
+                area_width = 800
+                area_height = 800
+                step_amt = 70
+                step_count = 0
+                x = (area_width * -1) / 2 + 50
+                y = (area_height * -1) / 2 + 50
+                for row in unique_cidrs:
+                    last_node_id['id'] = last_node_id['id'] + 1
+                    legend_colors[row['cidr']] = '#%02X%02X%02X' % (rando_color(),rando_color(),rando_color())
+                    legend_entry = {}
+                    legend_entry['id'] = str(last_node_id['id'])
+                    legend_entry['x'] = str(x)
+                    legend_entry['y'] = str(y  + (step_amt * step_count))
+                    legend_entry['title'] = "group_" + str(row['cidr'])
+                    legend_entry['label'] = "group_" + str(row['cidr'])
+                    legend_entry['group'] = "group_" + str(row['cidr'])
+                    legend_entry['value'] = "1"
+                    legend_entry['fixed'] = "true"
+                    legend_entry['physics'] = "false"
+                    legend_nodes.append(legend_entry)
+                    #legend_nodes.append('{ id: ' + str(last_node_id['id']) + ', x: ' + str(x) + ', y: ' + str(y  + (step_amt * step_count)) + ', title: "group_' + str(row['cidr']) + '", label: "group_' + str(row['cidr']) + '", group: "group_' + str(row['cidr']) + '", value: 1, fixed: true, physics: false},')
+                    step_count = step_count + 1
                 edges = Events.objects.filter(date='2020-10-14').values('src_ip_id','dst_ip_id','dstport','action').annotate(total=Count('id'))
                 # COOKIES AND STUFF!
                 #last_visit = request.session.get('last_page_visit', str(datetime.datetime.now()))
@@ -53,6 +81,8 @@ def index(request):
                     'nodes': nodes,
                     'edges': edges,
                     'num_visits': str(num_visits_pass),
+                    'legend_colors': legend_colors,
+                    'legend_nodes': legend_nodes
                 }
                 response = HttpResponse("")
                 response.set_cookie("num_visits", num_visits_pass)
